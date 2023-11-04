a2770751e8daa20885410c076c061a8c7248b865
Diogo Araujo <diogo.arauj@gmail.com>
2023-11-04 20:40:08 -0300

    First commit

---
+++ extrai_dados_rj.py
@@ -0,0 +1,24 @@
+import os
+from os.path import join
+import pandas as pd  
+from datetime import datetime, timedelta
+
+#intervalo de datas
+data_inicio = "2023-01-01"
+data_fim = "2023-02-01"
+
+city = 'RiodeJaneiro'
+key = 'KEY'
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+    f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
+
+dados = pd.read_csv(URL)
+print(dados.head())
+
+file_path = f'/Users/diogoaraujo/Desktop/data_pipeline/semana={data_inicio}'
+os.mkdir(file_path)
+
+dados.to_csv(file_path + 'dados_brutos_rj.csv')
+dados[['datetime','tempmin','temp','tempmax']].to_csv(file_path + 'temperatura_rj.csv')
+dados[['datetime','description','icon']].to_csv(file_path + 'condicoes_rj.csv')
